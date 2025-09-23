ce128fb192926899c90e80c3a0d6dc7aed7b9bc2
Ishita Kumari <ktaniishi01@gmail.com>
2025-09-23 14:11:34 +0530

    Knowledege Hub model Created

---
+++ server/src/models/knowledgeHubModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import jwt from "jwt";
+import bcrypt from "bcrypt";
+
+const knowledgeHubSchema = new mongoose.Schema({
+    category: {
+        type: "String",
+
+    },
+    title: {
+        ml: {
+            type: "String",
+        },
+        en: {
+            type: "String",
+        }
+    },
+    content: {
+        ml: {
+            type: "String",
+        },
+        en: {
+            type: "String",
+        }
+    },
+    type: {
+        type: "String",
+        enum: ["article", "scheme", "faq"],
+    },
+    relatedCrops: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Crop",
+    },
+    sourceLink: {
+        type: "String",
+    }
+},{timestamps: true})
+
+export default KnowledgeHub = mongoose.model("KnowledegeHub",knowledgeHubSchema);
